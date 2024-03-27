3727c9447808a824503c5f8c040489394d960630
Krypton96 <rizvan.shakhbiev@mail.ru>
2024-03-27 17:16:53 +0300

    Issue #6:Html Rendering viev

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from orders.views import order_pages
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', order_pages),
 ]
